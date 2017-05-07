29347927244a68bf70d24140d226a9ee4b8f1620
Zachary Viggiani <zach@bigmojo.net>
2017-05-07 13:24:29 -0400

    working on ajax

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $(document).ready(function () {
+
+    /* Number 1 and two */
+
+    $('#step12').on('click', function() {
+      console.log('Before .ajax');
+
+      $.ajax({
+        url:'https://bb-election-api.herokuapp.com/',
+        method: 'GET',
+        dataType: 'json',
+      });
+    });
+    console.log('After .ajax');
+  });
 
 });
