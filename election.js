d0f0ffc9c3b2af9eb2b4414ec7a550f82d29cc6f
Zachary Viggiani <zach@bigmojo.net>
2017-05-07 13:52:46 -0400

    done

--- election.js
+++ election.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
 
-  $(document).ready(function () {
+/* Number 1 and two */
 
-    /* Number 1 and two */
+$.ajax({    
 
-    $('#step12').on('click', function() {
-      console.log('Before .ajax');
+  console.log('loaded');
 
-      $.ajax({
-        url:'https://bb-election-api.herokuapp.com/',
-        method: 'GET',
-        dataType: 'json',
-      }).done(function(data) {
-      $('#step3456').append(data);
-      console.log('Got: ' + data)
-      
-      });
-    });
-    console.log('After .ajax');
-  });
+  url:'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'json',
+}).done(function(data) {
 
+     for (var i=0; i < data.candidates.length; i++) {
+       candidate = data.candidates[i]
+       var can=candidate.name + ' has ' + candidate.votes + ' votes.';
+       var item = $('<li>').html(can);
+       $('#attach-data').append(item);
+     }
+ });
 });
