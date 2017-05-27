e3e2b71a840d2ccc1b7c4ec5f3f00c2581d4e6b5
Zachary Viggiani <zach@bigmojo.net>
2017-05-27 19:26:16 -0400

    cats added

--- election.js
+++ election.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
+  console.log("success")
+  // Imagination!
 
-/* Number 1 and two */
-
-$.ajax({    
-
-  console.log('loaded');
-
-  url:'https://bb-election-api.herokuapp.com/',
-  method: 'GET',
-  dataType: 'json',
-}).done(function(data) {
-
-     for (var i=0; i < data.candidates.length; i++) {
-       candidate = data.candidates[i]
-       var can=candidate.name + ' has ' + candidate.votes + ' votes.';
-       var item = $('<li>').html(can);
-       $('#attach-data').append(item);
-     }
- });
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com',
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function(data) {
+      for (var i=0; i < data.candidates.length; i++) {
+        candidate = data.candidates[i]
+        var can=candidate.name + ' has ' + candidate.votes + ' votes.';
+        var item = $('<li>').html(can);
+        $('#everyCandidate').append(item);
+      }
+  });
 });
